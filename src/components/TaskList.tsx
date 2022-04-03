b047d4b8187f7b53b6612548df386b676d38da72
Allef-Reis <allef.fk@gmail.com>
2022-04-03 01:15:32 -0300

    conclusao do desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,29 +1,45 @@
-import { useState } from 'react'
-
-import '../styles/tasklist.scss'
-
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { useState } from "react";
+import "../styles/tasklist.scss";
+import { FiTrash, FiCheckSquare } from "react-icons/fi";
 
 interface Task {
   id: number;
   title: string;
   isComplete: boolean;
 }
-
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return; // primeira verificacao se title esta vazio
+    const newTask = {
+      //criar variavel contendo todo o conteudo que servirá para criar uma nova task
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+    setTasks((oldState) => [...oldState, newTask]); // pega setTasks e atualiza o estado com a nova task
+    setNewTaskTitle(""); // limpa o input
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const currentTaks = tasks.map(
+      (
+        task // usando map para varrer todos os itens encontrar o que possui o id
+      ) => (task.id == id ? { ...task, isComplete: !task.isComplete } : task) // encontrando o id é negado o iscomplete da task , caso o id for diferente retorna a task
+    );
+    setTasks(currentTaks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const filteredTasks = tasks.filter((task) => task.id != id); // fazendo um filtro ao inverso para que criar uma nova lista sem a task selecionada com base no id
+
+    setTasks(filteredTasks); // neste caso, como vou resetar a lista com um valor totalmente novo, sera totalmente recriada
   }
 
   return (
@@ -32,25 +48,32 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
-          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+          <button
+            type="submit"
+            data-testid="add-task-button"
+            onClick={handleCreateNewTask}
+          >
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
 
       <main>
         <ul>
-          {tasks.map(task => (
+          {tasks.map((task) => (
             <li key={task.id}>
-              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+              <div
+                className={task.isComplete ? "completed" : ""}
+                data-testid="task"
+              >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -61,14 +84,17 @@ export function TaskList() {
                 <p>{task.title}</p>
               </div>
 
-              <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+              <button
+                type="button"
+                data-testid="remove-task-button"
+                onClick={() => handleRemoveTask(task.id)}
+              >
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
-  )
+  );
 }
